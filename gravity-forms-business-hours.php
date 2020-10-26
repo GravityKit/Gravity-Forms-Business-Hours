d788175b93df3ae76dbb40f3b317e36f64dfe416
Vlad <vlad@gravityview.co>
2020-10-26 11:35:54 -0400

    Update plugin metadata

--- gravity-forms-business-hours.php
+++ gravity-forms-business-hours.php
@@ -1,36 +1,21 @@
 <?php
-/*
-Plugin Name: Gravity Forms Business Hours by GravityView
-Plugin URI: https://gravityview.co
-Description: Add a Business Hours field to your Gravity Forms form. Brought to you by <a href="https://gravityview.co">GravityView</a>, the best plugin for displaying Gravity Forms entries.
-Version: 2.1.3
-Author: GravityView
-Author URI: https://gravityview.co
-Text Domain: gravity-forms-business-hours
-Domain Path: /languages
-
-------------------------------------------------------------------------
-Copyright 2017 Katz Web Services, Inc.
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+/**
+ * Plugin Name: Gravity Forms Business Hours by GravityView
+ * Plugin URI:  https://wordpress.org/plugins/gravity-forms-business-hours/
+ * Description: Add a Business Hours field to your Gravity Forms form. Brought to you by <a href="https://gravityview.co">GravityView</a>, the best plugin for displaying Gravity Forms entries.
+ * Version:     2.1.3
+ * Author:      GravityView
+ * Author URI:  https://gravityview.co
+ * Text Domain: gravity-forms-business-hours
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Domain Path: /languages
  */
 
 add_action( 'gform_loaded', 'load_gf_business_hours' );
 
 function load_gf_business_hours() {
-	
+
 	//------------------------------------------
 	if ( class_exists("GFForms") ) {
 
@@ -42,5 +27,4 @@ function load_gf_business_hours() {
 
 		new GFBusinessHours;
 	}
-
 }
